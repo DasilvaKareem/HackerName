727d37ab2f71aa8d01a6a10e83cce94a4b191d6b
DasilvaKareem <kdasilva@memphis.edu>
2014-12-08 00:52:44 -0500

    Ok i am trying to stop the program from repeating the same name

--- hacker.js
+++ hacker.js
@@ -1,6 +1,23 @@
 
 function hackingTheSystem(){
 		var data = [];
-		var number = Math.floor((Math.random() * 10) + 1);
-		alert(number)
+		var number = Math.floor((Math.random() * 2) + 1);
+		var number2 = Math.floor((Math.random() * 2) + 1);
+		// Temp fix come back to fix lil dick nigga
+		var first = ['','Cool','0','Ultimate'];
+		var second = ['','hacker','leecher','Breaker'];
+		var name = first[number] + second[number2];
+		// Big ass prbolem
+		data.push(name);
+		do{
+			document.getElementById('name').innerHTML= name
+		}
+		while(name == data[0]){
+			document.getElementById('name').innerHTML= name
+			data = [];
+
+	}
+
+
+	console.log(data)
 }
